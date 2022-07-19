35d559a14e8d6c4b1a5c77ce14929eb7c6c6ab7c
Braylan Dodson <braylan1005@gmail.com>
2022-07-19 13:58:35 -0400

    commit tree 1da396fa1a2ae70499e705e5d524c0e1ea60d04e

---
+++ app.rb
@@ -0,0 +1,13 @@
+# app.rb
+
+# console logs and breaks the line
+puts "Hello World!"
+# console logs without breaking into a new line
+print "Hello World!"
+# console logs the array data on one line and breaks the line
+p [1,2,3]
+# "pretty-printing" console logs the array data and breaks the line for each entry in the array to be more easily read
+pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
+
+#code to pass the test:
+puts "Pass this test, please."
